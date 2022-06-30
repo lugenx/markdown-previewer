fdd608ef3c8412b475fed8effc97b6eb32f1b058
Elgun Asgar <elgunasgerov@gmail.com>
2022-06-30 19:06:39 -0400

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,42 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Editor } from "./Editor";
 import { Preview } from "./Preview";
-
-import "./App.css";
+import { defaultText } from "./defaultText";
 
 export const App = () => {
-  const defaultText = `
-  # Welcome to Markdown Previewer!
-  ## You can do cool stuff here
-    
-  For example link like [this](https://www.google.com/)!
-    
-  or... maybe some inline code
-  
-  \`const likeThis = 1\`
-  
-  or.. maybe a code block:
-  
-  \`\`\`
-  // this is multi-line code:
-  
-  function multiply(a, b) {
-    return a * b;
-  } 
-  \`\`\`
-  or.. a list:
-  - like
-  - this
-  - one
-    
-   or... an image like this good boi:
-   ![dogy](https://clubk9.com/wp-content/uploads/2018/04/Depositphotos_4953129_original-1-256x256.jpg)
-   
-  > or... a blockquote
-  
-  **and a bolded text of course!**`;
-
-  const [markedText, setMarkedText] = React.useState(defaultText);
+  const [markedText, setMarkedText] = useState(defaultText);
 
   function handleChange(event) {
     setMarkedText(event.target.value);
---
+++ src/defaultText.js
@@ -0,0 +1,30 @@
+export const defaultText = `
+# Welcome to Markdown Previewer!
+## You can do cool stuff here
+  
+For example link like [this](https://www.google.com/)!
+  
+or... maybe some inline code
+
+\`const likeThis = 1\`
+
+or.. maybe a code block:
+
+\`\`\`
+// this is multi-line code:
+
+function multiply(a, b) {
+  return a * b;
+} 
+\`\`\`
+or.. a list:
+- like
+- this
+- one
+  
+ or... an image like this good boi:
+ ![dogy](https://clubk9.com/wp-content/uploads/2018/04/Depositphotos_4953129_original-1-256x256.jpg)
+ 
+> or... a blockquote
+
+**and a bolded text of course!**`;
